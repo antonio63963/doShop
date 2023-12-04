4390378f77b62579d5b1138dbeaabb8239187eb7
Anton <60780638+antonio63963@users.noreply.github.com>
2023-12-04 12:28:20 +0200

    added provider categories

--- lib/doshop_app.dart
+++ lib/doshop_app.dart
@@ -1,7 +1,8 @@
 
-import 'package:doshop_app/screens/home_screen/view/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
+import 'providers/exports.dart';
 
 class DoshopApp extends StatelessWidget {
   const DoshopApp({super.key});
@@ -9,7 +10,7 @@ class DoshopApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(providers: [
-      ChangeNotifierProvider(create: (context) => ),
+      ChangeNotifierProvider(create: (context) => CategoriesProvider()),
     ]);
   }
 }
--- lib/providers/categories_provider.dart
+++ lib/providers/categories_provider.dart
@@ -1,22 +1,23 @@
+import 'package:flutter/material.dart';
 
-import 'package:doshop_app/db/initial_data.dart';
 import 'package:doshop_app/exports.dart';
 import 'package:get_it/get_it.dart';
 
 import '../db/exports.dart';
 
-class CategoriesProvider {
+class CategoriesProvider with ChangeNotifier {
   List<Category> _categories = [];
-  
+
+  List<Category> get categories {
+    return  _categories;
+  }
+
   Future<void> getCategoriesList() async {
     try {
       final categoriesList = await GetIt.I<AbstractDB>().getCategories();
-      if(categoriesList == null) {
+      if (categoriesList == null) {
         _categories = [];
-
       }
-    } catch(err) {
-
-    }
+    } catch (err) {}
   }
 }
