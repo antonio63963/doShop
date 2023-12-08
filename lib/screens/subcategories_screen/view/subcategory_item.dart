b7b3385bad5aada50b696615fa1f0b2808a4eae8
Anton <60780638+antonio63963@users.noreply.github.com>
2023-12-08 12:42:34 +0200

    clickable subItem

--- lib/screens/subcategories_screen/view/subcategory_item.dart
+++ lib/screens/subcategories_screen/view/subcategory_item.dart
@@ -8,42 +8,47 @@ class SubcategoryItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          color: Color(sub.colorBg!),
-          borderRadius: const BorderRadius.all(Radius.circular(5))),
-      width: double.infinity,
-      margin: const EdgeInsets.symmetric(vertical: 4),
-      padding: const EdgeInsets.all(16),
-      child: Row(children: [
-        SizedBox(
-          width: 134,
-          height: 90,
-          child: Image.asset(
-            sub.img ?? 'assets/img/categories/hobby.png',
-            fit: BoxFit.contain,
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4.0),
+      child: InkWell(
+        onTap: () {},
+        child: Ink(
+          decoration: BoxDecoration(
+            borderRadius: const BorderRadius.all(Radius.circular(5)),
+            color: Color(sub.colorBg!),
           ),
+          padding: const EdgeInsets.all(16),
+          child: Row(children: [
+            SizedBox(
+              width: 134,
+              height: 90,
+              child: Image.asset(
+                sub.img ?? 'assets/img/categories/hobby.png',
+                fit: BoxFit.contain,
+              ),
+            ),
+            const SizedBox(width: 8),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    sub.title,
+                    style: const TextStyle(
+                        color: MyColors.white,
+                        fontSize: 24,
+                        fontWeight: FontWeight.w800),
+                  ),
+                  Text(
+                    sub.subtitle ?? '',
+                    style: const TextStyle(color: MyColors.white, fontSize: 16),
+                  ),
+                ],
+              ),
+            )
+          ]),
         ),
-        const SizedBox(width: 8),
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                sub.title,
-                style: const TextStyle(
-                    color: MyColors.white,
-                    fontSize: 24,
-                    fontWeight: FontWeight.w800),
-              ),
-              Text(
-                sub.subtitle ?? '',
-                style: const TextStyle(color: MyColors.white, fontSize: 16),
-              ),
-            ],
-          ),
-        )
-      ]),
+      ),
     );
   }
 }
